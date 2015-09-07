187e5ec633ace788654537b3b034f23b6ab0ced9
Colin Rundel <rundel@gmail.com>
2015-09-07 10:37:18 -0400

    Update all subdirectory repos will a pull

--- update_repos.sh
+++ update_repos.sh
@@ -1,30 +1,9 @@
 #!/bin/sh
 
-if [ $# != 3 ]; then
-    echo "Usage: update_repos.sh <repo suffix> <file> <message>"
-    exit 1
-fi
-
-github='git@github.com:Sta523-Fa15/'
-teams=('Team1' 'Team2' 'Team3' 'Team4' 'Team5' 'Team6' 'Team7' 'Team8' 'Team9' 'Team10')
-
-suffix=${1}
-
-for team in ${teams[*]}
-do
-    folder="${team}${suffix}"
-    repo="${github}${folder}"
-
-    git clone ${repo}
-
-    cp ${2} ${folder}/
-
-    cd ${folder}
-    git add .
-    git commit -m ${3}
+for repo in */ ; do
+    echo "${repo}"
+    cd ${repo}
+    git pull
     cd ..
-
-    rm -rf ${folder}
-
-    break
+    echo "\n"
 done
